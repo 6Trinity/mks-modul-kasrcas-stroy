4a8023db5ce13060b3b2f811c35f7415cbbd33d1
Egor Martynov <bigbabyban228@gmail.com>
2025-08-11 04:09:38 +0500

    Added a site header and made an adaptive menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 import '@/styles/main.scss';
+
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollThreshold = 50;
+    const headers = document.querySelector('.appheader-content');
+    const navg = document.querySelector('.nav-ul-g');
+    const btnmenu = document.getElementById('btn');
+    const menu = document.querySelector('.appheader-menu-nav');
+
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > scrollThreshold) {
+            headers.classList.add('scrolled');
+            navg.classList.add('scrolled');
+        } else {
+            headers.classList.remove('scrolled');
+            navg.classList.remove('scrolled');
+        }
+    });
+
+    btnmenu.addEventListener('click', () =>{
+        menu.classList.toggle('active');
+    })
+});
