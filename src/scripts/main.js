60a156eccd04944ea64c839c1a35be4a8c3c2c21
Egor Martynov <bigbabyban228@gmail.com>
2025-08-12 02:12:22 +0500

    Completely finished the menu and cleaned up the classes as much as possible

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,13 @@ import '@/styles/main.scss';
 
 document.addEventListener('DOMContentLoaded', function() {
     const scrollThreshold = 50;
-    const header = this.querySelector('.appheader-content')
+    const header = this.querySelector('.appheader-content');
+    const menu = this.querySelector('.menu-nav');
+    const btnmenu = this.querySelector('.button-menu');
+
+    const toggleBodyScroll = (enable) => {
+        document.body.style.overflow = enable ? '' : 'hidden';
+    };
 
     window.addEventListener('scroll', () => {
         if (window.scrollY > scrollThreshold) {
@@ -14,6 +20,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
     btnmenu.addEventListener('click', () =>{
         btnmenu.classList.toggle('active');
-        menunav.classList.toggle('active');
+        menu.classList.toggle('active');
+        toggleBodyScroll(!menu.classList.contains('active'));
     })
+
+    document.querySelectorAll('.menu-nav a').forEach(link => {
+        link.addEventListener('click', (e) => {
+
+            menu.classList.remove('active');
+            btnmenu.classList.remove('active');
+            toggleBodyScroll(true);
+
+            const targetId = link.getAttribute('href');
+            if (targetId.startsWith('#')) {
+                e.preventDefault();
+                const targetElement = document.querySelector(targetId);
+                if (targetElement) {
+                    targetElement.scrollIntoView({
+                        behavior: 'smooth'
+                    });
+                }
+            }
+        });
+    });
 });
