ee485e24ac4f743a359e637e67143f9a11cd7146
Egor Martynov <bigbabyban228@gmail.com>
2025-08-11 05:47:01 +0500

    Improving menu adaptability

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,23 +2,16 @@ import '@/styles/main.scss';
 
 document.addEventListener('DOMContentLoaded', function() {
     const scrollThreshold = 50;
-    const headers = document.querySelector('.appheader-content');
-    const navg = document.querySelector('.nav-ul-g');
-    const btnmenu = document.getElementById('btn');
-    const menu = document.querySelector('.appheader-menu-nav');
-
+    const menu = document.querySelector('.appheader-menu');
+    const ulg = document.querySelector('.ul-g');
 
     window.addEventListener('scroll', () => {
         if (window.scrollY > scrollThreshold) {
-            headers.classList.add('scrolled');
-            navg.classList.add('scrolled');
+            menu.classList.add('scrolled');
+            ulg.classList.add('scrolled');
         } else {
-            headers.classList.remove('scrolled');
-            navg.classList.remove('scrolled');
+            menu.classList.remove('scrolled');
+            ulg.classList.remove('scrolled');
         }
     });
-
-    btnmenu.addEventListener('click', () =>{
-        menu.classList.toggle('active');
-    })
 });
