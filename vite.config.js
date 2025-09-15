39b0a241e69fc96b8dad7410192d75fa30623c82
Egor Martynov <bigbabyban228@gmail.com>
2025-09-15 05:00:46 +0500

    test pages

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,14 @@
 import { defineConfig } from 'vite'
 import path from 'path'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'src/index.html')
-    }
+    outDir: 'dist',
+    emptyOutDir: true
   },
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'pages/*.html', // Убрали 'src/' из пути!
-          dest: 'pages'
-        }
-      ]
-    })
-  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
-  },
+  }
 })
